25db98c9f60fe3fe726b6756c4e3601e8f661a8d
Vishal Menon <74124373+vmeno0020@users.noreply.github.com>
2022-06-10 15:38:20 -0700

    Edited load_data and create_db

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,17 +1,17 @@
 COPY MENU
-FROM '/extra/vmeno003/project/data/menu.csv'
+FROM '/extra/vmeno003/cs166project/data/menu.csv'
 WITH DELIMITER ';';
 
 COPY USERS
-FROM '/extra/vmeno003/project/data/users.csv'
+FROM '/extra/vmeno003/cs166project/data/users.csv'
 WITH DELIMITER ';';
 
 COPY ORDERS
-FROM '/extra/vmeno003/project/data/orders.csv'
+FROM '/extra/vmeno003/cs166project/data/orders.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE orders_orderid_seq RESTART 87257;
 
 COPY ITEMSTATUS
-FROM '/extra/vmeno003/project/data/itemStatus.csv'
+FROM '/extra/vmeno003/cs166project/data/itemStatus.csv'
 WITH DELIMITER ';';
 
