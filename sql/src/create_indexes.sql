0250ed3f3f15d6330d43752b0f407f4ddab8bc5a
Vishal Menon <74124373+vmeno0020@users.noreply.github.com>
2022-06-10 20:48:15 -0700

    Update create_indexes.sql

--- sql/src/create_indexes.sql
+++ sql/src/create_indexes.sql
@@ -5,3 +5,23 @@ ON orders
 CREATE INDEX index2
 ON ItemStatus
 ( orderid );
+
+CREATE INDEX user_login_ind ON Users USING BTREE (login);
+CREATE INDEX user_phonenum_ind ON Users USING BTREE (phoneNum); 
+CREATE INDEX user_type_ind ON Users USING BTREE (type); 
+
+CREATE INDEX menu_itemname_ind ON Menu USING BTREE (itemName); 
+CREATE INDEX menu_type_ind ON Menu USING BTREE (type); 
+CREATE INDEX menu_price_ind ON Menu USING BTREE (price); 
+CREATE INDEX menu_desc_ind ON Menu USING BTREE (description); 
+
+CREATE INDEX orders_orderid_ind ON Orders USING BTREE (orderid); 
+CREATE INDEX orders_login_ind ON Orders USING BTREE (login); 
+CREATE INDEX orders_paid_ind ON Orders USING BTREE (paid); 
+CREATE INDEX orders_timerecieved_ind ON Orders USING BTREE (timestampRecieved);
+CREATE INDEX orders_total_ind ON Orders USING BTREE (total); 
+
+CREATE INDEX itemstatus_orderid_ind ON ItemStatus USING BTREE (orderid); 
+CREATE INDEX itemstatus_itemname_ind ON ItemStatus USING BTREE (itemName); 
+CREATE INDEX itemstatus_lastupdated_ind ON ItemStatus USING BTREE (lastUpdated); 
+CREATE INDEX itemstatus_comments_ind ON ItemStatus USING BTREE (comments); 
